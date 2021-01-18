8db3ce8cec9389646a6556ca09164f114a8e150b
Ngô Hữu Nam <ngohuunam@gmail.com>
2021-01-18 21:54:30 +0700

    ok

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,11 +4,23 @@ import { useEffect, useState } from 'react';
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Copyright from '../src/Copyright';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Link from '../src/Link';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+}));
 
 export default function Login() {
+  const classes = useStyles();
   const { firebaseClient } = useAuth();
   const [isFirebaseClientReady, setIsFirebaseClientReady] = useState(false);
 
@@ -16,10 +28,17 @@ export default function Login() {
 
   return (
     <Container maxWidth='sm'>
-      <Box>
+      <Box mt={10} display="flex" flexDirection="column" alignItems="center">
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
         {isFirebaseClientReady ? <FirebaseAuth fpc={firebaseClient} /> : <CircularProgress />}
-        <Link href="/">Go to the main page</Link>
-        <Copyright />
+        <Link href="/">
+          <Button color="primary"> Go back </Button>
+        </Link>
       </Box>
     </Container>
   );
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,29 +1,22 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
+// import Container from '@material-ui/core/Container';
+// import Typography from '@material-ui/core/Typography';
+// import Box from '@material-ui/core/Box';
+// import ProTip from '../src/ProTip';
 import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+// import Copyright from '../src/Copyright';
 import nookies from 'nookies'
 import { firebaseAdmin } from "../provider/firebase/firebase-admin";
 // import { firebaseClient } from "../provider/firebase/firebase-client";
+import Dashboard from '../components/Dashboard/Dashboard'
 
-import { GetServerSideProps, InferGetServerSidePropsType, GetServerSidePropsContext } from "next";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 
-const Profile = (
-  props: InferGetServerSidePropsType<typeof getServerSideProps>
-) => (
-  <Container maxWidth='sm'>
-    <Box>
-      <Typography variant='h4' component='h4' gutterBottom>
-        {props.message!}
-      </Typography>
-      <Link href='/'>Go to the main page</Link>
-      <ProTip />
-      <Copyright />
-    </Box>
-  </Container>
+const Profile = () => (
+  <>
+    <Dashboard />
+    <Link href='/'>Go to the main page</Link>
+  </>
 );
 
 
